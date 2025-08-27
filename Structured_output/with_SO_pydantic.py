ca4e566e255183b686a1f37d32e5a400d89d1dd7
Pravat Patra <pravatpatra1997@gmail.com>
2025-08-28 00:49:23 +0530

    SO commit

---
+++ Structured_output/with_SO_pydantic.py
@@ -0,0 +1,44 @@
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from pydantic import BaseModel,Field
+from typing import Optional,Literal
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id='deepseek-ai/DeepSeek-V3.1',
+    task='text-generation'
+)
+
+model=ChatHuggingFace(llm=llm)
+
+class Review(BaseModel):
+
+    key_theme : list[str]=Field(description="Write down all the key theme of this review")
+    summary : str =Field(description="Give me the summary of this review")
+    sentiment : Literal['pos','neg']=Field(description="Give me thr sentiment either positive or negative")
+    pros : Optional[list[str]]=Field(default=None,description="Write down all the pros of this review")
+    cons : Optional[list[str]]=Field(default=None,description="Write down all the cons of this review")
+    name : Optional[str]= Field(default=None,description="Write the name of who write this review")
+
+structured_model=model.with_structured_output(Review,method='json_mode')
+result=structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Pravat Patra
+""")
+dict_result=dict(result)
+print(dict_result)
+
+### It will give ERROR
+### NotImplementedError: Pydantic schema is not supported for function calling
+### But if we add one parameter in out with_structured_output(method='json mode') then it will work. But output is not optimistic.
