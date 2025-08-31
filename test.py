c4521a86e09b0e0cfc3197dd1d42262869b42008
Pravat Patra <pravatpatra1997@gmail.com>
2025-08-31 23:25:21 +0530

    Retriever commit

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 
+"""llm=HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-20b",
+    task="text-generation"
+)
+
+llm_model=ChatHuggingFace(llm=llm)
+
+res=llm_model.invoke("Describe about Rohit Sharma in 2 lines")
+
+print(res.content)"""
 
